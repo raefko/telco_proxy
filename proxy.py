328cab2f3654ac09adb1ac202ceac38513c1123d
Nabih <nabih@fuzzinglabs.com>
2024-10-17 16:20:47 +0200

    change 0.0.0.0 to localhost

--- proxy.py
+++ proxy.py
@@ -6,7 +6,7 @@ import select
 import re
 
 # Define the proxy server's IP and port
-PROXY_IP = "0.0.0.0"
+PROXY_IP = "127.0.0.1"
 PROXY_PORT = 5060
 PROXY_UDP_PORT = 5062
 TARGET_IP = "80.156.100.67"
